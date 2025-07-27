405def43d076c0a96dcf8ea5299dfeecc868e38e
IngPau <paulaleonardo289@gmail.com>
2025-07-26 20:42:38 -0600

    Creación ventana usuarios

---
+++ Usuarios.Designer.cs
@@ -0,0 +1,432 @@
+﻿namespace WinFormsApp1
+{
+    partial class Usuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Usuarios));
+            panel1 = new Panel();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            BtnAgregar = new Button();
+            pictureBox7 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            label8 = new Label();
+            BuscarNombre = new ListBox();
+            SeleccionarRol = new ListBox();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            dataGridView6 = new DataGridView();
+            dataGridView4 = new DataGridView();
+            dataGridView3 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            label7 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(24, 52, 70);
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(BtnAgregar);
+            panel1.Controls.Add(pictureBox7);
+            panel1.Controls.Add(pictureBox6);
+            panel1.Controls.Add(pictureBox5);
+            panel1.Controls.Add(pictureBox4);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(BuscarNombre);
+            panel1.Controls.Add(SeleccionarRol);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(dataGridView6);
+            panel1.Controls.Add(dataGridView4);
+            panel1.Controls.Add(dataGridView3);
+            panel1.Controls.Add(dataGridView2);
+            panel1.Controls.Add(dataGridView1);
+            panel1.Controls.Add(label1);
+            panel1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            panel1.Location = new Point(216, 77);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(984, 577);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 7.8F, FontStyle.Bold);
+            button3.Location = new Point(857, 541);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 22;
+            button3.Text = "ELIMINAR";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 7.8F, FontStyle.Bold);
+            button2.Location = new Point(757, 541);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 21;
+            button2.Text = "GUARDAR";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 7.8F, FontStyle.Bold);
+            button1.Location = new Point(657, 541);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 20;
+            button1.Text = "MODIFICAR";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // BtnAgregar
+            // 
+            BtnAgregar.Font = new Font("Segoe UI", 7.8F, FontStyle.Bold);
+            BtnAgregar.Location = new Point(557, 541);
+            BtnAgregar.Name = "BtnAgregar";
+            BtnAgregar.Size = new Size(94, 29);
+            BtnAgregar.TabIndex = 19;
+            BtnAgregar.Text = "AGREGAR";
+            BtnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(879, 498);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(38, 38);
+            pictureBox7.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox7.TabIndex = 18;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(788, 498);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(38, 38);
+            pictureBox6.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox6.TabIndex = 17;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(687, 497);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(38, 38);
+            pictureBox5.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox5.TabIndex = 16;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(588, 497);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(38, 38);
+            pictureBox4.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox4.TabIndex = 15;
+            pictureBox4.TabStop = false;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(65, 62);
+            label8.Name = "label8";
+            label8.Size = new Size(879, 20);
+            label8.TabIndex = 14;
+            label8.Text = "_________________________________________________________________________________________________________________________________________________";
+            // 
+            // BuscarNombre
+            // 
+            BuscarNombre.AccessibleName = "Buscar Nombre";
+            BuscarNombre.BackColor = Color.FromArgb(141, 153, 174);
+            BuscarNombre.FormattingEnabled = true;
+            BuscarNombre.Location = new Point(65, 94);
+            BuscarNombre.Name = "BuscarNombre";
+            BuscarNombre.Size = new Size(150, 24);
+            BuscarNombre.TabIndex = 13;
+            BuscarNombre.SelectedIndexChanged += BuscarNombre_SelectedIndexChanged;
+            // 
+            // SeleccionarRol
+            // 
+            SeleccionarRol.AccessibleName = "Seleccionar Rol";
+            SeleccionarRol.BackColor = Color.FromArgb(141, 153, 174);
+            SeleccionarRol.FormattingEnabled = true;
+            SeleccionarRol.Location = new Point(265, 94);
+            SeleccionarRol.Name = "SeleccionarRol";
+            SeleccionarRol.Size = new Size(150, 24);
+            SeleccionarRol.TabIndex = 12;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label6.Location = new Point(851, 135);
+            label6.Name = "label6";
+            label6.Size = new Size(38, 20);
+            label6.TabIndex = 11;
+            label6.Text = "ROL";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label5.Location = new Point(672, 135);
+            label5.Name = "label5";
+            label5.Size = new Size(88, 20);
+            label5.TabIndex = 10;
+            label5.Text = "CONTACTO";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label4.Location = new Point(460, 135);
+            label4.Name = "label4";
+            label4.Size = new Size(178, 20);
+            label4.TabIndex = 9;
+            label4.Text = "FECHA DE NACIMIENTO";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label3.Location = new Point(265, 135);
+            label3.Name = "label3";
+            label3.Size = new Size(74, 20);
+            label3.TabIndex = 8;
+            label3.Text = "NOMBRE";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label2.Location = new Point(94, 135);
+            label2.Name = "label2";
+            label2.Size = new Size(25, 20);
+            label2.TabIndex = 7;
+            label2.Text = "ID";
+            // 
+            // dataGridView6
+            // 
+            dataGridView6.BackgroundColor = Color.FromArgb(238, 238, 238);
+            dataGridView6.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView6.Location = new Point(800, 158);
+            dataGridView6.Name = "dataGridView6";
+            dataGridView6.RowHeadersWidth = 51;
+            dataGridView6.Size = new Size(151, 333);
+            dataGridView6.TabIndex = 6;
+            // 
+            // dataGridView4
+            // 
+            dataGridView4.BackgroundColor = Color.FromArgb(238, 238, 238);
+            dataGridView4.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView4.Location = new Point(643, 158);
+            dataGridView4.Name = "dataGridView4";
+            dataGridView4.RowHeadersWidth = 51;
+            dataGridView4.Size = new Size(151, 333);
+            dataGridView4.TabIndex = 4;
+            // 
+            // dataGridView3
+            // 
+            dataGridView3.BackgroundColor = Color.FromArgb(238, 238, 238);
+            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView3.Location = new Point(460, 158);
+            dataGridView3.Name = "dataGridView3";
+            dataGridView3.RowHeadersWidth = 51;
+            dataGridView3.Size = new Size(177, 333);
+            dataGridView3.TabIndex = 3;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.BackgroundColor = Color.FromArgb(238, 238, 238);
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(163, 158);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 51;
+            dataGridView2.Size = new Size(291, 333);
+            dataGridView2.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = Color.FromArgb(238, 238, 238);
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(65, 158);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(92, 333);
+            dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(24, 52, 70);
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(238, 238, 238);
+            label1.Location = new Point(65, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(169, 41);
+            label1.TabIndex = 0;
+            label1.Text = "USUARIOS";
+            label1.Click += label1_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(830, 25);
+            label7.Name = "label7";
+            label7.Size = new Size(256, 31);
+            label7.TabIndex = 1;
+            label7.Text = "USER ADMINISTRATOR";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(216, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(56, 59);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(1092, 9);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(66, 62);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(1164, 25);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(36, 36);
+            pictureBox3.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox3.TabIndex = 4;
+            pictureBox3.TabStop = false;
+            // 
+            // Usuarios
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(141, 153, 174);
+            ClientSize = new Size(1247, 681);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label7);
+            Controls.Add(panel1);
+            Name = "Usuarios";
+            Text = "Usuarios";
+            Load += Usuarios_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private DataGridView dataGridView6;
+        private DataGridView dataGridView4;
+        private DataGridView dataGridView3;
+        private DataGridView dataGridView2;
+        private DataGridView dataGridView1;
+        private Label label7;
+        private ListBox BuscarNombre;
+        private ListBox SeleccionarRol;
+        private Label label8;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Button BtnAgregar;
+    }
+}
