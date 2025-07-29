5d1d87bd7d705d0d2731213ed2445633a02f070d
vennuus <bsaldanas1@miumg.edu.gt>
2025-07-29 07:55:04 -0600

    validaciones de pilotos y transporte

--- obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04afe860da19403231245d3c5d392640fafba10a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d7049bc6010458015dd4e33d21b3b6922b4db24")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
